a5c20cbfaae64781b5bfec4ddde12551f714cee4
DESHAN K.D.M <130528796+DHARAKA-METH@users.noreply.github.com>
2025-12-21 09:50:34 +0530

    implement error middleware

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,21 @@ import jobRouter from "./routes/job.route.js";
 import applicationRouter from "./routes/application.route.js";
 import companyRouter from "./routes/company.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middleware.js/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/jobs", jobRouter);
 app.use("/api/applications", applicationRouter);
 app.use("/api/companies", companyRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to job portal Backend!");
